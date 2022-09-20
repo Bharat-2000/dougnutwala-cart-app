2dbaff57fdeba2c4f44b833139f28b53f637f805
Bharat-2000 <yadeep999@gmail.com>
2022-09-20 16:33:09 +0530

    first commit with cart update error => hardcoded valued not changing

---
+++ js/app.js
@@ -0,0 +1,81 @@
+// its a IIFE => immediately invoked function expression
+
+(function(){
+    const cartInfo = document.getElementById('cart-info');
+    const cart = document.getElementById('cart');
+
+    cartInfo.addEventListener('click', function(){
+        cart.classList.add('show-cart');
+    });
+})();
+
+// add items to cart
+
+(function(){ 
+    const cartBtn = document.querySelectorAll('.store-item-icon');
+    cartBtn.forEach(function(btn){
+        btn.addEventListener('click',function(event){
+            // console.log(event.target);
+            if(event.target.parentElement.classList.contains('store-item-icon')){
+                let fullPath = event.target.parentElement.previousElementSibling.src;
+                let pos = fullPath.indexOf('img') + 3;
+                let partPath = fullPath.slice(pos);
+
+                const item = {};
+                item.img = `img-cart${partPath}`;
+                let name = event.target.parentElement.parentElement.nextElementSibling.children[0].children[0].textContent;
+                item.name = name;
+
+                let price = event.target.parentElement.parentElement.nextElementSibling.children[0].children[1].textContent;
+
+                let finalPrice = price.slice(1).trim();
+
+                item.price = finalPrice;
+
+                const cartItem = document.createElement('div');
+                cartItem.classList.add('cart-item', 'd-flex', 'justify-content-between', 'text-capitalize', 'my-3');
+
+                cartItem.innerHTML = `
+                    <img src="${item.img}" class="img-fluid rounded-circle" id="item-img" alt="">
+                    <div class="item-text">
+                        <p id="cart-item-title" class="font-weight-bold mb-0">${item.name}</p>
+                        <span>$</span>
+                        <span id="cart-item-price" class="cart-item-price" class="mb-0">${item.price}</span>
+                    </div>
+                    <a href="#" id='cart-item-remove' class="cart-item-remove">
+                    <i class="fas fa-trash"></i>
+                    </a>
+                `;
+
+                // select card starts here
+                const cart = document.getElementById('cart');
+                const total = document.querySelector('.cart-total-container');
+
+                cart.insertBefore(cartItem, total);
+                alert('item added to the cart');
+                showTotals();
+            }
+        });
+    });
+
+    showTotals = () =>{
+        let total = [];
+        const items = document.querySelectorAll('.cart-item-price');
+        
+        items.forEach(function(item){
+            total.push(parseFloat(item.textContent));
+        });
+        // console.log(total);
+        const totalMoney = total.reduce(function(total, items){
+            total += items;
+            return total;
+        }, 0); //this is a reduce method used to basically reduce the selected target with a given paremeters of the total and the item to which we want to reduce
+        const finalMoney = totalMoney.toFixed(2);
+        // console.log(finalMoney);
+
+        document.getElementById('cart-total').textContext = finalMoney;
+        document.querySelectorAll('.item-total').textContent = finalMoney;
+        document.getElementById('item-count').textContnt = total.length;
+        console.log(total.length);
+    }
+})();
